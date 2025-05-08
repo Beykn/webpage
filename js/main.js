b4f57e492a98699612cb8df7d80746bb21f29df1
Beyhan Kandemir <beyhankandemir@Beyhan-MacBook-Air.local>
2025-05-08 09:59:40 +0300

    last sprint

--- js/main.js
+++ js/main.js
@@ -10,8 +10,23 @@ $(document).ready(function() {
         arrows: true,
         fade: true
     });
-    
+
     if ($('#tabs').length) {
         $('#tabs').tabs();
     }
+
+    // Fetch news
+    $.ajax({
+        url: 'data/news.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            data.forEach(function(item) {
+                $('#news-feed').append('<p><strong>' + item.title + '</strong>: ' + item.description + '</p>');
+            });
+        },
+        error: function() {
+            $('#news-feed').append('<p>Failed to load news.</p>');
+        }
+    });
 });
